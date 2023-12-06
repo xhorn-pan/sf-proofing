e149fbf8be93d4416703d03d7792819615450f86
Xinghua Pan <23328176+xhorn-pan@users.noreply.github.com>
2023-12-06 15:44:43 -0500

    more exercises in basic and induction

--- Basics.v
+++ Basics.v
@@ -290,3 +290,39 @@ Proof.
 Qed.
 
 (* More Exercises TBC*)
+
+(* Binary numberals *)
+Inductive bin : Type :=
+| Z
+| B0 (n: bin)
+| B1 (n: bin).
+
+Fixpoint incr (m:bin) : bin :=
+  match m with
+  | Z => B1 Z
+  | B0 m1 => B1 m1
+  | B1 m1 => B0 (incr m1)
+  end.
+
+Fixpoint bin_to_nat (m: bin) : nat :=
+  match m with
+  | Z => O
+  | B1 Z => S O
+  | B0 m' =>   (bin_to_nat m') + (bin_to_nat m')
+  | B1 m' => S ((bin_to_nat m') + (bin_to_nat m'))
+  end.
+
+Example test_bin_incr1 : (incr (B1 Z)) = B0 (B1 Z).
+Proof. reflexivity. Qed.
+
+Example test_bin_incr2 : (incr (B0 (B1 Z))) = B1 (B1 Z).
+Proof. reflexivity. Qed.
+Example test_bin_incr3 : (incr (B1 (B1 Z))) = B0 (B0 (B1 Z)).
+Proof. reflexivity. Qed.
+
+Example test_bin_incr4 : bin_to_nat (B0 (B1 Z)) = 2.
+Proof. reflexivity. Qed.
+Example test_bin_incr5 : bin_to_nat (incr (B1 Z)) = 1 + bin_to_nat (B1 Z).
+Proof. reflexivity. Qed.
+Example test_bin_incr6 : bin_to_nat (incr (incr (B1 Z))) = 2 + bin_to_nat (B1 Z).
+Proof. reflexivity. Qed.
--- Induction.v
+++ Induction.v
@@ -111,3 +111,181 @@ Qed.
 (* _Informal proofs are algorithms; formal proofs are code *)
 
 (* More Exercises TBC*)
+Theorem add_shuffle3 : forall n m p : nat, n + (m + p) = m + (n + p).
+Proof. (* use assert, not induction *)
+  intros.
+  Abort.
+
+Check mult_n_Sm.
+Lemma mult_n_Sm' : forall n m: nat, n * m + n = n * S m.
+Proof.
+  intros n m.
+  induction n.
+  induction m.
+  - reflexivity.
+  - reflexivity.
+  - rewrite <- plus_n_Sm.
+    simpl.
+    rewrite <- IHn.
+    rewrite add_assoc.
+    reflexivity.
+Qed.
+
+Theorem mul_comm : forall m n: nat, m * n = n * m.
+Proof.
+  intros m n.
+  induction m.
+  induction n.
+  - reflexivity.
+  - simpl.
+    rewrite -> mul_0_r.
+    reflexivity.
+  - simpl.
+    rewrite <- mult_n_Sm'.
+    rewrite -> IHm.
+    rewrite -> add_comm.
+    reflexivity.
+Qed.
+
+
+Check leb.
+Theorem plus_leb_compat_l : forall n m p: nat, n <=? m = true -> (p + n) <=? (p + m) = true.
+Proof.
+  intros.
+  induction p.
+  - simpl.
+    apply H.
+  - simpl.
+    apply IHp.
+Qed.
+
+(* more exercises *)
+Theorem leb_refl : forall n: nat, (n <=? n) = true.
+Proof.
+  intros.
+  induction n.
+  - reflexivity.
+  - simpl.
+    rewrite -> IHn.
+    reflexivity.
+Qed.
+
+Theorem zero_neqb_S : forall n: nat, 0 =? (S n) = false.
+Proof.
+  intros.
+  induction n.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Theorem andb_false_r : forall b: bool, andb b false = false.
+Proof.
+  intros.
+  destruct b.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Theorem S_neqb_0 : forall n: nat, (S n) =? 0 = false.
+Proof.
+  intros.
+  induction n.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Theorem mult_1_l : forall n: nat, 1 * n = n.
+Proof.
+  intros.
+  destruct n.
+  - reflexivity.
+  - simpl. rewrite add_0_r. reflexivity.
+Qed.
+
+Theorem all3_spec : forall b c : bool,
+    orb (andb b c) (orb (negb b) (negb c)) = true.
+Proof.
+  intros.
+  destruct c.
+  - destruct b.
+    + reflexivity.
+    + reflexivity.
+  - destruct b.
+    + reflexivity.
+    + reflexivity.
+Qed.
+
+Print add_comm.
+
+Require Import Arith.
+Theorem mult_plus_distr_r : forall n m p : nat,
+    (n + m) * p = (n * p) + (m * p).
+Proof.
+  intros.
+  induction p.
+  - repeat rewrite mul_0_r.
+    reflexivity.
+  - repeat rewrite <- mult_n_Sm'.
+    rewrite -> IHp.
+    repeat rewrite -> add_assoc.
+    ring. (* do not know exactly how, but it works. *)
+Qed.
+
+Theorem mult_assoc : forall n m p: nat, n * (m * p) = (n * m) * p.
+Proof.
+  intros.
+  induction n.
+  - reflexivity.
+  - simpl.
+    rewrite -> IHn.
+    rewrite ->  mult_plus_distr_r.
+    reflexivity.
+Qed.
+
+(* nat to bin and back to nat *)
+Inductive bin : Type := | Z | B0 (n : bin) | B1 ( n: bin).
+
+Fixpoint incr (m:bin) : bin :=
+  match m with
+  | Z => B1 Z
+  | B0 m1 => B1 m1
+  | B1 m1 => B0 (incr m1)
+  end.
+
+Fixpoint bin_to_nat (m: bin) : nat :=
+  match m with
+  | Z => O
+  | B1 Z => S O
+  | B0 m' =>   (bin_to_nat m') + (bin_to_nat m')
+  | B1 m' => S ((bin_to_nat m') + (bin_to_nat m'))
+  end.
+
+Theorem bin_to_nat_pres_incr : forall b : bin, bin_to_nat(incr b) = 1 + bin_to_nat b.
+Proof.
+  intros.
+  induction b.
+  - reflexivity.
+  - simpl.
+    destruct b.
+    + reflexivity.
+    + reflexivity.
+    + reflexivity.
+  - simpl.
+    destruct b.
+    + reflexivity.
+    + rewrite IHb.
+      rewrite plus_n_Sm.
+      simpl. reflexivity.
+    + rewrite IHb.
+      rewrite plus_n_Sm.
+      reflexivity.
+Qed.
+
+
+(* 
+Fixpoint nat_to_bin (n: nat) : bin :=
+  match n with
+  | O => Z
+  | S O => B1 Z
+  | S  => 
+             *)
